3d20b5fc14b5aab7cf2e39d36f92af3185ccf560
Gary <garylin0969@gmail.com>
2022-07-02 23:16:26 +0800

    0702

--- routes/member.js
+++ routes/member.js
@@ -7,94 +7,11 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
-router.get('/login', async (req, res)=>{
+router.get('/login', async (req, res) => {
     res.render('login');
 });
 
-// 會員登入
-router.post('/login', upload.none(), async (req, res) => {
-    const output = {
-        success: false,
-        bollen: false,
-        new: false,
-        grade: 'low',
-        token: '',
-        info: null,
-        code: 0,
-        error: '',
-    };
-
-    const [rs] = await db.query('SELECT * FROM member WHERE mem_account=?', [
-        req.body.account,
-    ]);
-
-    if (!rs.length) {
-        output.error = '帳密錯誤';
-        output.code = 401;
-        return res.json(output);
-    }
-
-    const row = rs[0];
-
-    // 資料庫密碼解密
-    const compareResult = await bcrypt.compare(req.body.password, row.mem_password);
-
-    if (! compareResult) {
-        output.error = '帳密錯誤';
-        output.code = 402;
-        return res.json(output);
-    }
-
-    if (row.mem_bollen == 1) {
-        output.bollen = true;
-    } else {
-        output.error = '帳號已被停用';
-        output.code = 403;
-        return res.json(output);
-    }
-
-    const { sid, mem_account, mem_nickname, mem_created_at, mem_avatar } = row;
-
-    const now = new Date();
-    const newOld = now - mem_created_at;
-
-    // 86400000是24小時的毫秒數
-    if (newOld < 86400000) {
-        output.new = true;
-    }
-
-    output.success = true;
-    output.info = { mem_account };
-
-    // 未加密前
-    // output.token = {
-    //     sid,
-    //     mem_account,
-    //     mem_nickname,
-    //     new:output.new,
-    //     mem_created_at,
-    //     mem_avatar,
-    //     grade:output.grade,
-    // };
-
-    // 進行加密讓前端頁面看不出來
-    output.token = jwt.sign(
-        {
-            sid,
-            mem_account,
-            mem_nickname,
-            new: output.new,
-            mem_created_at,
-            mem_avatar,
-            grade: output.grade,
-        },
-        process.env.JWT_KEY
-    );
-
-    res.json(output);
-});
-
-// 會員註冊
+// 註冊
 router.post('/add', upload.none(), async (req, res) => {
     const output = {
         success: false,
@@ -168,7 +85,6 @@ router.post('/add', upload.none(), async (req, res) => {
         birthday,
         address,
         avatar,
-        created,
     } = req.body;
 
     // 密碼加密再存進資料庫
@@ -194,9 +110,169 @@ router.post('/add', upload.none(), async (req, res) => {
     res.json(output);
 });
 
-// 刪除帳號
+// 登入
+router.post('/login', upload.none(), async (req, res) => {
+    const output = {
+        success: false,
+        bollen: false,
+        new: false,
+        grade: 'low',
+        token: '',
+        info: null,
+        code: 0,
+        error: '',
+    };
+
+    const [rs] = await db.query('SELECT * FROM member WHERE mem_account=?', [
+        req.body.account,
+    ]);
+
+    if (!rs.length) {
+        output.error = '帳密錯誤';
+        output.code = 401;
+        return res.json(output);
+    }
+
+    const row = rs[0];
+
+    // 資料庫密碼解密
+    const compareResult = await bcrypt.compare(
+        req.body.password,
+        row.mem_password
+    );
+
+    if (!compareResult) {
+        output.error = '帳密錯誤';
+        output.code = 402;
+        return res.json(output);
+    }
+
+    if (row.mem_bollen == 1) {
+        output.bollen = true;
+    } else {
+        output.error = '帳號已被停用';
+        output.code = 403;
+        return res.json(output);
+    }
+
+    const { sid, mem_account, mem_nickname, mem_created_at, mem_avatar } = row;
+
+    const now = new Date();
+    const newOld = now - mem_created_at;
+
+    // 86400000是24小時的毫秒數
+    if (newOld < 86400000) {
+        output.new = true;
+    }
+
+    output.success = true;
+    output.info = { mem_account };
+
+    // 未加密前
+    // output.token = {
+    //     sid,
+    //     mem_account,
+    //     mem_nickname,
+    //     new:output.new,
+    //     mem_created_at,
+    //     mem_avatar,
+    //     grade:output.grade,
+    // };
+
+    // 進行加密讓前端頁面看不出來
+    output.token = jwt.sign(
+        {
+            sid,
+            mem_account,
+            mem_nickname,
+            new: output.new,
+            mem_created_at,
+            mem_avatar,
+            grade: output.grade,
+        },
+        process.env.JWT_KEY
+    );
+
+    res.json(output);
+});
+
+// 修改
+router.post('/edit', upload.none(), async (req, res) => {
+    const output = {
+        success: false,
+        code: 0,
+        error: '',
+    };
+
+    //後端檢查用
+    const schema = Joi.object({
+        //字串類型
+        name: Joi.string()
+            //最少2個字
+            .min(2)
+            //必填欄位
+            .required()
+            //欄位變數名稱用中文顯示
+            .label('姓名必填'),
+        nickname: Joi.any(),
+        email: Joi.string().email(),
+        mobile: Joi.string(),
+        account: Joi.string().required(),
+        //可以是任何類型
+        birthday: Joi.any(),
+        address: Joi.any(),
+    });
+
+    const find = schema.validate(req.body, { abortEarly: false });
+
+    if (find.error) {
+        (output.code = 401), (output.error = '資料有錯誤');
+        return res.json(output);
+    }
+
+    const sql =`UPDATE 'member' SET 'mem_name'=?,'mem_nickname'=?,'mem_account'=?,'mem_email'=?,'mem_mobile'=?,'mem_birthday'=?,'mem_address'=?, WHERE 'sid'=${res.locals.user.sid}`;
+
+    // 如果沒填 給空字串
+    if (!req.body.nickname) {
+        req.body.nickname = '';
+    }
+    if (!req.body.email) {
+        req.body.email = '';
+    }
+    if (!req.body.mobile) {
+        req.body.mobile = '';
+    }
+    if (!req.body.address) {
+        req.body.address = '';
+    }
+
+    const {
+        name,
+        nickname,
+        account,
+        email,
+        mobile,
+        birthday,
+        address,
+    } = req.body;
+
+    const [result] = await db.query(sql, [
+        name,
+        nickname,
+        account,
+        email,
+        mobile,
+        birthday,
+        address,
+    ]);
+
+    output.success = true;
+    res.json(output);
+});
+
+// 刪除
 router.delete('/delete', (req, res) => {
-    const sql = db.query("DELETE FROM `member` WHERE sid=?", [
+    const sql = db.query('DELETE FROM `member` WHERE sid=?', [
         res.locals.user.sid,
     ]);
 });
