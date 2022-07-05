27b2b4ce66efcbc47576255c9d45e47c5588f52e
GaryL1n <garylin0969@gmail.com>
2022-07-05 14:23:19 +0800

    會員上傳頭貼

--- routes/member.js
+++ routes/member.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const db = require('../modules/connect_db');
 const router = express.Router();
-const upload = require('../modules/upload-images');
 const Joi = require('joi');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
+const upload = require('../modules/upload-avatar');
 
 router.get('/login', async (req, res) => {
     res.render('login');
@@ -303,6 +303,10 @@ router.post('/password', upload.none(), async (req, res) => {
 // 刪除帳號
 router.delete('/', (req, res) => {
     const sql = db.query(`DELETE FROM member WHERE sid=${res.locals.user.sid}`);
+});
+
+router.post('/upload', upload.single('avatar'), (req, res)=>{
+    res.json(req.file);
 });
 
 module.exports = router;
