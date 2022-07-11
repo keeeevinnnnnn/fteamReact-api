3b4d5b6fce362e536fa7d04407be66ef22018d19
NathanLai <a403440322@gmail.com>
2022-07-11 17:47:50 +0800

    update sql database name

--- modules/connect_db.js
+++ modules/connect_db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password:'',
-    database: 'fteam',
+    password: '',
+    database: 'fteampro',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
