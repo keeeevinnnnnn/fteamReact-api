e1639f7b24de4796342343829da3dfccd17be91c
kevin wu <kevin85627@gmail.com>
2022-07-25 03:45:12 +0800

    1

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const db = require("../modules/connect_db");
 const router = express.Router();
 
 // 全部商品
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   let output = {
     perPage: 15,
     page: 1,
@@ -14,8 +14,10 @@ router.get("/", async (req, res) => {
     query: {},
     rows: [],
   };
-  let page = +req.query.page || 1;
+  let page = +req.body.filter.page || 1;
   let where = " WHERE 1=1 ";
+  let orderField = "sid";
+  let sort = "asc";
 
   if (page < 1) {
     output.code = 410;
@@ -35,190 +37,45 @@ router.get("/", async (req, res) => {
       return output;
     }
 
-    const sql02 = `SELECT * FROM product ${where} ORDER BY sid LIMIT ${
-      (page - 1) * output.perPage
-    }, ${output.perPage}`;
-    const [r2] = await db.query(sql02);
-    output.rows = r2;
-  }
-  output.code = 200;
-  output = { ...output, page, totalRows, totalPages };
+    sort != null ? req.body.filter.sort : sort;
 
-  res.json(output);
-});
-
-// ToolBox Skateboard filter
-router.get("/skateboard", async (req, res) => {
-  let output = {
-    perPage: 15,
-    page: 1,
-    totalRows: 0,
-    totalPages: 0,
-    code: 0, // 辨識狀態
-    error: "",
-    query: {},
-    rows: [],
-  };
-  let page = +req.query.page || 1;
-  let where = " WHERE 1=1 ";
-
-  if (page < 1) {
-    output.code = 410;
-    output.error = "頁碼太小";
-    return output;
-  }
-
-  const sql01 = `SELECT COUNT(1) totalRows FROM product ${where} `;
-  const [[{ totalRows }]] = await db.query(sql01);
-  let totalPages = 0;
-  if (totalRows) {
-    totalPages = Math.ceil(totalRows / output.perPage);
-    if (page > totalPages) {
-      output.totalPages = totalPages;
-      output.code = 420;
-      output.error = "頁碼太大";
-      return output;
+    if (orderField != null) {
+      orderField = req.body.filter.orderField;
     }
 
-    const sql02 = `SELECT * FROM product ${where} AND category_id IN(2,3) ORDER BY sid LIMIT ${
-      (page - 1) * output.perPage
-    }, ${output.perPage}`;
-    const [r2] = await db.query(sql02);
-    output.rows = r2;
-  }
-  output.code = 200;
-  output = { ...output, page, totalRows, totalPages };
-
-  res.json(output);
-});
-
-// ToolBox Decks filter
-router.get("/decks", async (req, res) => {
-  let output = {
-    perPage: 15,
-    page: 1,
-    totalRows: 0,
-    totalPages: 0,
-    code: 0, // 辨識狀態
-    error: "",
-    query: {},
-    rows: [],
-  };
-  let page = +req.query.page || 1;
-  let where = " WHERE 1=1 ";
-
-  if (page < 1) {
-    output.code = 410;
-    output.error = "頁碼太小";
-    return output;
-  }
-
-  const sql01 = `SELECT COUNT(1) totalRows FROM product ${where} `;
-  const [[{ totalRows }]] = await db.query(sql01);
-  let totalPages = 0;
-  if (totalRows) {
-    totalPages = Math.ceil(totalRows / output.perPage);
-    if (page > totalPages) {
-      output.totalPages = totalPages;
-      output.code = 420;
-      output.error = "頁碼太大";
-      return output;
+    switch (orderField) {
+      case "categoryId":
+        orderField = "category_id";
+        break;
+      case "brand":
+        orderField = "brand";
+        break;
+      case "color":
+        orderField = "color";
+        break;
+      default:
+        orderField = "sid";
+        break;
     }
 
-    const sql02 = `SELECT * FROM product ${where} AND category_id=4 ORDER BY sid LIMIT ${
-      (page - 1) * output.perPage
-    }, ${output.perPage}`;
-    const [r2] = await db.query(sql02);
-    output.rows = r2;
-  }
-  output.code = 200;
-  output = { ...output, page, totalRows, totalPages };
+    let sql02 = `SELECT * FROM product ${where}`;
 
-  res.json(output);
-});
-
-// ToolBox Trucks filter
-router.get("/trucks", async (req, res) => {
-  let output = {
-    perPage: 15,
-    page: 1,
-    totalRows: 0,
-    totalPages: 0,
-    code: 0, // 辨識狀態
-    error: "",
-    query: {},
-    rows: [],
-  };
-  let page = +req.query.page || 1;
-  let where = " WHERE 1=1 ";
-
-  if (page < 1) {
-    output.code = 410;
-    output.error = "頁碼太小";
-    return output;
-  }
-
-  const sql01 = `SELECT COUNT(1) totalRows FROM product ${where} `;
-  const [[{ totalRows }]] = await db.query(sql01);
-  let totalPages = 0;
-  if (totalRows) {
-    totalPages = Math.ceil(totalRows / output.perPage);
-    if (page > totalPages) {
-      output.totalPages = totalPages;
-      output.code = 420;
-      output.error = "頁碼太大";
-      return output;
+    if (req.body.filter.categoryId != null) {
+      let categoryId = req.body.filter.categoryId;
+      sql02 = sql02 + " and category_id = " + categoryId;
     }
 
-    const sql02 = `SELECT * FROM product ${where} AND category_id=5 ORDER BY sid LIMIT ${
+    // if (req.query.brand != "null") {
+    //   let brand = req.query.brand;
+    //   sql02 = sql02 + " and brand = " + brand;
+    // }
+    console.log(page);
+
+    const sql04 = ` ORDER BY ${orderField} ${sort} LIMIT ${
       (page - 1) * output.perPage
     }, ${output.perPage}`;
-    const [r2] = await db.query(sql02);
-    output.rows = r2;
-  }
-  output.code = 200;
-  output = { ...output, page, totalRows, totalPages };
 
-  res.json(output);
-});
-
-// ToolBox Wheels filter
-router.get("/wheels", async (req, res) => {
-  let output = {
-    perPage: 15,
-    page: 1,
-    totalRows: 0,
-    totalPages: 0,
-    code: 0, // 辨識狀態
-    error: "",
-    query: {},
-    rows: [],
-  };
-  let page = +req.query.page || 1;
-  let where = " WHERE 1=1 ";
-
-  if (page < 1) {
-    output.code = 410;
-    output.error = "頁碼太小";
-    return output;
-  }
-
-  const sql01 = `SELECT COUNT(1) totalRows FROM product ${where} `;
-  const [[{ totalRows }]] = await db.query(sql01);
-  let totalPages = 0;
-  if (totalRows) {
-    totalPages = Math.ceil(totalRows / output.perPage);
-    if (page > totalPages) {
-      output.totalPages = totalPages;
-      output.code = 420;
-      output.error = "頁碼太大";
-      return output;
-    }
-
-    const sql02 = `SELECT * FROM product ${where} AND category_id=6 ORDER BY sid LIMIT ${
-      (page - 1) * output.perPage
-    }, ${output.perPage}`;
-    const [r2] = await db.query(sql02);
+    const [r2] = await db.query(sql02 + sql04);
     output.rows = r2;
   }
   output.code = 200;
