1f772a26a8718ef6cfe92aa196bdc27c14fe2bdd
kevin wu <kevin85627@gmail.com>
2022-08-08 20:37:06 +0800

    123

--- routes/lesson.js
+++ routes/lesson.js
@@ -15,21 +15,21 @@ router.get("/", upload.none(), async (req, res) => {
   // console.log(req.query.location);
   if (req.query.location === "忠孝館") {
     const sql =
-      "SELECT lesson.*,dance_category.type,teacher_category.name FROM lesson JOIN teacher_category on teacher_category.sid = lesson.teacher_id JOIN dance_category ON dance_category.sid = lesson.dance_id WHERE lesson.location = '忠孝館'";
+      "SELECT lesson.*,dance_category.type,teacher_category.teacher_name FROM lesson JOIN teacher_category on teacher_category.sid = lesson.teacher_id JOIN dance_category ON dance_category.sid = lesson.dance_id WHERE lesson.location = '忠孝館'";
     const [r] = await db.query(sql);
     res.json(r);
   }
   if (req.query.location === "板橋民生旗艦館") {
     // console.log(req.query.location);
     const sql =
-      "SELECT lesson.*,dance_category.type,teacher_category.name FROM lesson JOIN teacher_category on teacher_category.sid = lesson.teacher_id JOIN dance_category ON dance_category.sid = lesson.dance_id WHERE lesson.location = '板橋民生旗艦館'";
+      "SELECT lesson.*,dance_category.type,teacher_category.teacher_name FROM lesson JOIN teacher_category on teacher_category.sid = lesson.teacher_id JOIN dance_category ON dance_category.sid = lesson.dance_id WHERE lesson.location = '板橋民生旗艦館'";
     const [r] = await db.query(sql);
     res.json(r);
   }
   if (req.query.location === "台中精誠館") {
     // console.log(req.query.location);
     const sql =
-      "SELECT lesson.*,dance_category.type,teacher_category.name FROM lesson JOIN teacher_category on teacher_category.sid = lesson.teacher_id JOIN dance_category ON dance_category.sid = lesson.dance_id WHERE lesson.location = '台中精誠館'";
+      "SELECT lesson.*,dance_category.type,teacher_category.teacher_name FROM lesson JOIN teacher_category on teacher_category.sid = lesson.teacher_id JOIN dance_category ON dance_category.sid = lesson.dance_id WHERE lesson.location = '台中精誠館'";
     const [r] = await db.query(sql);
     res.json(r);
   }
